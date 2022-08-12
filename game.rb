afaf8a64c96b8388d14d8a7a130471f003367622
Mark Delgado <markdelgado.md@hotmail.com>
2022-08-12 11:55:00 -0400

    add database

--- game.rb
+++ game.rb
@@ -1,3 +1,18 @@
+require 'securerandom'
+require 'sqlite3'
+
+db = SQLite3::Database.new('game.db')
+
+# Create a table
+rows = db.execute <<-SQL
+  create table numbers (
+    name varchar(30),
+    val int
+  );
+SQL
+p rows
+
+
 class Player
     attr_accessor :name, :email, :password, :deck
     def initialize(name, email, password, deck=[])
@@ -8,16 +23,18 @@ class Player
     end
 end
 
-class Card 
-    attr_accessor :name, :top, :left, :bottom, :right, :element=nil, :img=nil
-    def initialize(name, top, left, bottom, right, element=nil, img=nil)
-        @name = name 
-        @top = top
-        @left = left
-        @bottom = bottom
-        @right = right
-        @element = element
-        @img = img
-    end
+#in order to save this information we would need to create  a cards table
+class Card
+  attr_accessor :name, :top, :left, :bottom, :right, :element, :img
+  def initialize(name, top, left, bottom, right, element=nil, img=nil)
+    @name = name 
+    @top = top
+    @left = left
+    @bottom = bottom
+    @right = right
+    @element = element
+    @img = img
+    @card_id = SecureRandom.uuid
+  end
 end
 
